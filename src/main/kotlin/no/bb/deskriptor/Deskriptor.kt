7b537da2890e647f3c9dcdf986fe204d96e1382f
Torkel Rogstad <torkel@rogstad.io>
2023-01-09 12:44:02 +0100

    multi: add basic access log (#4)

--- src/main/kotlin/no/bb/deskriptor/Deskriptor.kt
+++ src/main/kotlin/no/bb/deskriptor/Deskriptor.kt
@@ -1,10 +1,13 @@
 package no.bb.deskriptor
 
-import io.grpc.Server
+import io.grpc.*
+import io.grpc.ForwardingServerCall.SimpleForwardingServerCall
 import io.grpc.protobuf.services.ProtoReflectionService
-import io.grpc.ServerBuilder
+import mu.KotlinLogging
 import no.bb.deskriptor.service.Service
 import org.bitcoindevkit.*
+
+val logger = KotlinLogging.logger { }
 
 fun main(args: Array<String>) {
     if ("-h" in args || "--help" in args) {
@@ -15,14 +18,44 @@ fun main(args: Array<String>) {
     val port = 5000
 
     val service = Service(Network.BITCOIN)
-    val server: Server = ServerBuilder
-        .forPort(port)
-        .addService(service)
-        .addService(ProtoReflectionService.newInstance())
-        .build()
+    val server: Server =
+        ServerBuilder.forPort(port)
+            .addService(service)
+            .addService(ProtoReflectionService.newInstance())
+            .intercept(ServerLogInterceptor()).build()
 
     server.start()
-    println("Server started, listening on $port")
+    logger.info("Server started, listening on $port")
 
     server.awaitTermination()
+}
+
+
+class ServerLogInterceptor : ServerInterceptor {
+    override fun <ReqT, RespT> interceptCall(
+        call: ServerCall<ReqT, RespT>?, headers: Metadata?, next: ServerCallHandler<ReqT, RespT>
+    ): ServerCall.Listener<ReqT> {
+        val listener = LogDelegate(call!!, headers!!)
+        return next.startCall(listener, headers)
+    }
+}
+
+class LogDelegate<ReqT, RespT>(private val delegate: ServerCall<ReqT, RespT>, private val headers: Metadata) :
+    SimpleForwardingServerCall<ReqT, RespT>(delegate) {
+
+    override fun close(status: Status?, trailers: Metadata?) {
+        val method = delegate.methodDescriptor.fullMethodName
+        val desc = if (status?.code == Status.Code.OK) "OK" else {
+            "${status?.code}: ${status?.description}"
+        }
+
+        val metadataKeyValue = headers.keys().joinToString(separator = "\t", prefix = "\t") { keyName ->
+            val key = Metadata.Key.of(keyName, Metadata.ASCII_STRING_MARSHALLER)
+            val value = headers.get(key)
+            "$keyName=$value"
+        }
+
+        logger.info { "$method: $desc $metadataKeyValue" }
+        super.close(status, trailers)
+    }
 }
